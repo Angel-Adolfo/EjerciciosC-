7c55e4f9e9bd056aaf3140d9d2aa8c2597dcda78
amendez2019362 <amendez-2019362@kinal.edu.gt>
2024-09-16 11:16:34 -0600

    Inicio de proyecto semana 3

---
+++ Proyectos/ProyectoSemana3/Alumno.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoSemana3
+{
+    class Alumno
+    {
+
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Identificacion { get; set; }
+        public string Carrera { get; set; }
+        public double[] Notas { get; set; }
+
+        public Alumno(string nombre, string apellido, string identificacion, string carrera, double[] notas) 
+        {
+            Nombre = nombre;
+            Apellido = apellido;
+            Identificacion = identificacion;
+            Carrera = carrera;
+            Notas = notas;
+        }
+    }
+}
---
+++ Proyectos/ProyectoSemana3/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoSemana3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Alumno[] alumnos = new Alumno[]
+            {
+            new Alumno("Angel", "Mendez", "101", "Ingeniería", new double[] { 8.9, 7.0, 10.0, 8.0, 7.5 }),
+            new Alumno("Sofia", "Arriola", "102", "Medicina", new double[] { 9.0, 8.5, 9.5, 10.0, 8.0 }),
+            new Alumno("Elmer", "Santos", "103", "Ingeniería", new double[] { 0.5, 5.0, 6.0, 7.0, 6.0 }),
+            new Alumno("Alejandro", "Max", "104", "Dibujo", new double[] { 0.5, 2.5, 9.5, 8.3, 5.5 }),
+            new Alumno("Andrew", "Mendez", "105", "Electricidad", new double[] { 5.0, 8.5, 9.0, 10.0, 10.0 }),
+            };
+
+            Console.WriteLine("Informacion de todos los alumnos:");
+            foreach (var alumno in alumnos)
+            {
+                Console.WriteLine($"{alumno.Nombre} {alumno.Apellido} {alumno.Identificacion} {alumno.Carrera} {alumno.Promedio():0.00}");
+            }
+
+            Console.WriteLine(" ");
+            Console.WriteLine("Alumnos con promedio mayor a 6:");
+            foreach (var alumno in alumnos)
+            {
+                if (alumno.Promedio() >= 6)
+                {
+                    Console.WriteLine($"{alumno.Nombre} {alumno.Apellido}, ID: {alumno.Identificacion}");
+                }
+            }
+
+            double promedioGeneral = 0;
+            foreach (var alumno in alumnos)
+            {
+                promedioGeneral += alumno.Promedio();
+            }
+            promedioGeneral /= alumnos.Length;
+            Console.WriteLine(" ");
+            Console.WriteLine($"Promedio general de la clase es: {promedioGeneral:0.00}");
+
+            Console.ReadKey();
+        }
+    }
+}
